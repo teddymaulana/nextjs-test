1d1b14c41eae8321b9b8875a4cc252d9e80d7207
Teddy Maulana <maulanateddy99@gmail.com>
2022-11-23 20:50:44 +0800

    updates

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -8,7 +8,6 @@ const Blog = () => (
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
     </p>
 
